260402cf6994ac0e27f82637b788b9ff71c4ea01
Lucas van der Ploeg <lucas.ploeg@gmail.com>
2014-01-23 10:08:45 +0100

    Eerste try met usb-kabel.

--- src/com/example/kassascanner/MainActivity.java
+++ src/com/example/kassascanner/MainActivity.java
@@ -1,27 +1,39 @@
 package com.example.kassascanner;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import android.app.Activity;
-import android.view.Menu;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-import android.os.Bundle;
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class MainActivity extends Activity implements OnClickListener {
 	private Button scanBtn;
 	private TextView formatTxt, contentTxt;
+
+	private static String SERVER_IP = "192.168.42.148";
+	private String EAN;
+	private Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		context = this.getApplicationContext();
 
 		scanBtn = (Button) findViewById(R.id.scan_button);
 		formatTxt = (TextView) findViewById(R.id.scan_format);
@@ -51,14 +63,36 @@ public class MainActivity extends Activity implements OnClickListener {
 		if (scanningResult != null) {
 			String scanContent = scanningResult.getContents();
 			String scanFormat = scanningResult.getFormatName();
-			
+
 			formatTxt.setText("FORMAT: " + scanFormat);
 			contentTxt.setText("CONTENT: " + scanContent);
+			EAN = scanContent;
+			try {
+				Socket s = new Socket("192.168.42.148", 21111);
+				String message = "blablabla";
+
+				PrintWriter outp = null;
+				BufferedReader inp = null;
+				String serverMsg = null;
+
+				outp = new PrintWriter(s.getOutputStream(), true);
+				inp = new BufferedReader(new InputStreamReader(
+						s.getInputStream()));
+				serverMsg = inp.readLine();
+
+				if (message != null) {
+					// convo.append(message + "\n");
+					outp.println(message);
+					serverMsg = inp.readLine();
+					// convo.append(serverMsg + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		} else {
 			Toast toast = Toast.makeText(getApplicationContext(),
 					"No scan data received!", Toast.LENGTH_SHORT);
 			toast.show();
 		}
 	}
-
 }
